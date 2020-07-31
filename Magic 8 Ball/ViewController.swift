72e8f26ee98fd608ef5556ec518be8bf17d141f5
Clayton Watkins <watkinclayton17@yahoo.com>
2020-07-30 17:11:07 -0700

    Finished Project.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - IBOutlet
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // MARK: - Properties
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
-
+    
+    // MARK: - IBAction
+    @IBAction func askButtonTapped(_ sender: Any) {
+        let currentImage = randomImage(imageArray: ballArray)
+        imageView.image = currentImage
+    }
+    
+    // MARK: - Private Functions
+    private func randomImage(imageArray: [UIImage]) -> UIImage {
+        let randomIndex = randomNumber(closedRange: 0...imageArray.count - 1)
+        print(randomIndex)
+        return imageArray[randomIndex]
+    }
+    
+    private func randomNumber(closedRange: ClosedRange<Int>) -> Int{
+        return Int.random(in: closedRange)
+    }
 }
 
